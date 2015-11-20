8251f700b772f139ce44f5a38b40e72fc48d09c8
Dmitry <seedmanc@yahoo.com>
2015-11-20 13:17:40 +0300

    the js

---
+++ booru augmentation project.js
@@ -0,0 +1,14 @@
+﻿// ==UserScript==
+// @name		Booru Augmentation Project
+// @description	Enhance your basic booru experience
+// @version	1.0
+// @author		Seedmanc
+// @include	http://*.booru.org/index.php?page=post*
+// @grant 		none 
+// @run-at		document-body
+// @noframes
+// ==/UserScript==
+
+alert('start')
+
+document.addEventListener('DOMContentLoaded',function(){alert('dom');});
